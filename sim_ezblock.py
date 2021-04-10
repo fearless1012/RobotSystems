60de2fb753b54ce7658b9f92a7c7c685218d0425
Ramya <fearless_ramya@yahoo.co.in>
2021-04-09 20:47:08 -0700

    smbus_error
    Commit before smbus error resolve to work this out in linux machine

---
+++ sim_ezblock.py
@@ -0,0 +1,236 @@
+import time
+import logging
+import smbus, math
+from .i2c import I2C
+
+timer = [
+            {
+                "arr": 0
+            }
+        ] * 4
+
+
+class PWM(I2C):
+    REG_CHN = 0x20
+    REG_FRE = 0x30
+    REG_PSC = 0x40
+    REG_ARR = 0x44
+
+    ADDR = 0x14
+
+    CLOCK = 72000000
+
+    def __init__(self, channel, debug="critical"):
+        super().__init__()
+        if isinstance(channel, str):
+            if channel.startswith("P"):
+                channel = int(channel[1:])
+            else:
+                raise ValueError("PWM channel should be between [P1, P14], not {0}".format(channel))
+        try:
+            self.send(0x2C, self.ADDR)
+            self.send(0, self.ADDR)
+            self.send(0, self.ADDR)
+        except IOError:
+            self.ADDR = 0x15
+
+        self.debug = debug
+        self._debug("PWM address: {:02X}".format(self.ADDR))
+        self.channel = channel
+        self.timer = int(channel / 4)
+        self.bus = smbus.SMBus(1)
+        self._pulse_width = 0
+        self._freq = 50
+        self.freq(50)
+
+    def i2c_write(self, reg, value):
+        value_h = value >> 8
+        value_l = value & 0xff
+        self._debug("i2c write: [0x%02X, 0x%02X, 0x%02X, 0x%02X]" % (self.ADDR, reg, value_h, value_l))
+        self.send([reg, value_h, value_l], self.ADDR)
+
+    def freq(self, *freq):
+        if len(freq) == 0:
+            return self._freq
+        else:
+            self._freq = int(freq[0])
+            # [prescaler,arr] list
+            result_ap = []
+            # accuracy list
+            result_acy = []
+            # middle value for equal arr prescaler
+            st = int(math.sqrt(self.CLOCK / self._freq))
+            # get -5 value as start
+            st -= 5
+            # prevent negetive value
+            if st <= 0:
+                st = 1
+            for psc in range(st, st + 10):
+                arr = int(self.CLOCK / self._freq / psc)
+                result_ap.append([psc, arr])
+                result_acy.append(abs(self._freq - self.CLOCK / psc / arr))
+            i = result_acy.index(min(result_acy))
+            psc = result_ap[i][0]
+            arr = result_ap[i][1]
+            self._debug("prescaler: %s, period: %s" % (psc, arr))
+            self.prescaler(psc)
+            self.period(arr)
+
+    def prescaler(self, *prescaler):
+        if len(prescaler) == 0:
+            return self._prescaler
+        else:
+            self._prescaler = int(prescaler[0]) - 1
+            reg = self.REG_PSC + self.timer
+            self._debug("Set prescaler to: %s" % self._prescaler)
+            self.i2c_write(reg, self._prescaler)
+
+    def period(self, *arr):
+        global timer
+        if len(arr) == 0:
+            return timer[self.timer]["arr"]
+        else:
+            timer[self.timer]["arr"] = int(arr[0]) - 1
+            reg = self.REG_ARR + self.timer
+            self._debug("Set arr to: %s" % timer[self.timer]["arr"])
+            self.i2c_write(reg, timer[self.timer]["arr"])
+
+    def pulse_width(self, *pulse_width):
+        if len(pulse_width) == 0:
+            return self._pulse_width
+        else:
+            self._pulse_width = int(pulse_width[0])
+            reg = self.REG_CHN + self.channel
+            self.i2c_write(reg, self._pulse_width)
+
+    def pulse_width_percent(self, *pulse_width_percent):
+        global timer
+        if len(pulse_width_percent) == 0:
+            return self._pulse_width_percent
+        else:
+            self._pulse_width_percent = pulse_width_percent[0]
+            temp = self._pulse_width_percent / 100.0
+            #            print(temp)
+            pulse_width = temp * timer[self.timer]["arr"]
+            self.pulse_width(pulse_width)
+
+
+def test():
+    import time
+    p = PWM(0)
+    # p.debug = 'debug'
+    p.period(1000)
+    p.prescaler(10)
+    # p.pulse_width(2048)
+    while True:
+        for i in range(0, 4095, 10):
+            p.pulse_width(i)
+            print(i)
+            time.sleep(1 / 4095)
+        time.sleep(1)
+        for i in range(4095, 0, -10):
+            p.pulse_width(i)
+            print(i)
+            time.sleep(1 / 4095)
+        time.sleep(1)
+
+
+class _Basic_class(object):
+    _class_name = '_Basic_class'
+    DEBUG_LEVELS = {'debug': logging.DEBUG,
+              'info': logging.INFO,
+              'warning': logging.WARNING,
+              'error': logging.ERROR,
+              'critical': logging.CRITICAL,
+              }
+    DEBUG_NAMES = ['critical', 'error', 'warning', 'info', 'debug']
+
+    def __init__(self):
+        self._debug_level = 0
+        self.logger = logging.getLogger(self._class_name)
+        self.ch = logging.StreamHandler()
+        form = "%(asctime)s	[%(levelname)s]	%(message)s"
+        self.formatter = logging.Formatter(form)
+        self.ch.setFormatter(self.formatter)
+        self.logger.addHandler(self.ch)
+        self._debug    = self.logger.debug
+        self._info     = self.logger.info
+        self._warning  = self.logger.warning
+        self._error    = self.logger.error
+        self._critical = self.logger.critical
+
+    @property
+    def debug(self):
+        return self._debug_level
+
+    @debug.setter
+    def debug(self, debug):
+        if debug in range(5):
+            self._debug_level = self.DEBUG_NAMES[debug]
+        elif debug in self.DEBUG_NAMES:
+            self._debug_level = debug
+        else:
+            raise ValueError('Debug value must be 0(critical), 1(error), 2(warning), 3(info) or 4(debug), not \"{0}\".'.format(debug))
+        self.logger.setLevel(self.DEBUG_LEVELS[self._debug_level])
+        self.ch.setLevel(self.DEBUG_LEVELS[self._debug_level])
+        self._debug('Set logging level to [%s]' % self._debug_level)
+
+    def run_command(self, cmd):
+        import subprocess
+        p = subprocess.Popen(
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        result = p.stdout.read().decode('utf-8')
+        status = p.poll()
+        # print(result)
+        # print(status)
+        return status, result
+
+    def map(self, x, in_min, in_max, out_min, out_max):
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+class Servo(_Basic_class):
+    MAX_PW = 2500
+    MIN_PW = 500
+    _freq = 50
+
+    def __init__(self, pwm):
+        super().__init__()
+        self.pwm = pwm
+        self.pwm.period(4095)
+        prescaler = int(float(self.pwm.CLOCK) / self.pwm._freq / self.pwm.period())
+        self.pwm.prescaler(prescaler)
+        # self.angle(90)
+
+    # angle ranges -90 to 90 degrees
+    def angle(self, angle):
+        if not (isinstance(angle, int) or isinstance(angle, float)):
+            raise ValueError("Angle value should be int or float value, not %s" % type(angle))
+        if angle < -90:
+            angle = -90
+        if angle > 90:
+            angle = 90
+        High_level_time = self.map(angle, -90, 90, self.MIN_PW, self.MAX_PW)
+        self._debug("High_level_time: %f" % High_level_time)
+        pwr = High_level_time / 20000
+        self._debug("pulse width rate: %f" % pwr)
+        value = int(pwr * self.pwm.period())
+        self._debug("pulse width value: %d" % value)
+        self.pwm.pulse_width(value)
+
+
+def test():
+    #from ezblock import PWM
+    print("Test")
+    p = PWM("P0")
+    s0 = Servo(p)
+    s0.debug = "debug"
+    s0.angle(90)
+
+def test2():
+    p = PWM("P0")
+    while True:
+        p.pulse_width_percent(50)
+
+
+if __name__ == "__main__":
+    test()

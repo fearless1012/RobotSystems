0c191952eca182215d4b11cfe8553064cefed124
fearless1012 <fearless_ramya@yahoo.co.in>
2021-04-17 10:34:57 -0700

    Final for Motor Command

--- picar_driver.py
+++ picar_driver.py
@@ -1,85 +1,98 @@
-import picarx_improved
+from picarx_improved import picarx_improved
 from time import sleep
 
-def move_forward():
-    for i in range(10):
-        picarx_improved.forward(80,30)
-    picarx_improved.stop()
+class picar_driver(picarx_improved):
+    def __init__(self):
+        super().__init__()
+        self.angle = 0
+        self.speed = 0
+        return
 
-def move_backward():
-    for i in range(10):
-        picarx_improved.backward(80)
-    picarx_improved.stop()
+    def move_forward(self):
+        for i in range(10):
+            self.forward(self.speed, self.angle)
+        self.stop()
 
-def move_left():
-    for i in range(100):
-        picarx_improved.set_dir_servo_angle(-30)
-    for i in range(10):
-        picarx_improved.forward(40,-30)
-    picarx_improved.stop()
+    def move_backward(self):
+        for i in range(10):
+            self.backward(self.speed)
+        self.stop()
 
-def move_right():
-    for i in range(100):
-        picarx_improved.set_dir_servo_angle(30)
-    for i in range(10):
-        picarx_improved.forward(40,30)
-    picarx_improved.stop()
+    def move_left(self):
+        for i in range(100):
+            self.set_dir_servo_angle(self.angle)
+        for i in range(10):
+            self.forward(self.speed, self.angle)
+        self.stop()
 
-def parallel_parking(direction='l'):
-    if direction == 'l':
-        dir_i = -1
-    else:
-        dir_i=1
+    def move_right(self):
+        for i in range(100):
+            self.set_dir_servo_angle(self.angle)
+        for i in range(10):
+            self.forward(self.speed, self.angle)
+        self.stop()
 
-    picarx_improved.set_dir_servo_angle(10*dir_i)
-    picarx_improved.backward(10)
-    sleep(1)
-    picarx_improved.set_dir_servo_angle(-10*dir_i)
-    picarx_improved.forward(10, 0)
-    sleep(1)
-    picarx_improved.stop()
+    def parallel_parking(self, direction='l'):
+        if direction == 'l':
+            dir_i = -1
+        else:
+            dir_i = 1
+        for i in range(15):
+            self.set_dir_servo_angle(30*dir_i)
+        for i in range(10):
+            self.forward(40)
+        for i in range(30):
+            self.set_dir_servo_angle(-30*dir_i)
+        for i in range(10):
+            self.forward(10, -30*dir_i)
+        self.stop()
 
-def k_turning():
-    for i in range(1000):
-        picarx_improved.set_dir_servo_angle(-10)
-    #picarx_improved.backward(10)
-    #sleep(1)
-    #picarx_improved.stop()
-    #picarx_improved.set_dir_servo_angle(20)
-    #picarx_improved.forward(10, 0)
-    sleep(1)
-    picarx_improved.stop()
-    picarx_improved.forward(10, 0.60)
-    sleep(1)
-    picarx_improved.stop()
+    def k_turning(self, direction = 'l'):
+        if direction == 'l':
+            dir_i = -1
+        else:
+            dir_i = 1
+        for i in range(15):
+            self.set_dir_servo_angle(30 * dir_i)
+        for i in range(10):
+            self.backward(40)
+        for i in range(10):
+            self.set_dir_servo_angle(30 * dir_i)
+        for i in range(10):
+            self.forward(10, -30 * dir_i)
+        self.stop()
 
-def keyboard_input():
-    speed =0
-    angle =0
-    input_choice = input("used 'w-s-a-d' for 'forward-backward-left-right' maneuvering. 'e' to exit maneuvering, 'p' to park and 'k' for k_turn")
+    def keyboard_input(self):
+        speed =0
+        angle =0
+        input_choice = input("used 'w-s-a-d' for 'forward-backward-left-right' maneuvering. 'e' to exit maneuvering, 'p' to park and 'k' for k_turn")
 
-    while input_choice != 'e':
-        if input_choice == 'w':
-            move_forward()
-        elif input_choice == 's':
-            move_backward()
-        elif input_choice == 'a':
-            move_left()
-        elif input_choice == 'd':
-            move_right()
-        elif input_choice == 'p':
-            get_dir = input("'r' for right park and 'l' for left park")
-            parallel_parking(get_dir)
-            picarx_improved.stop()
-        elif input_choice == 'k':
-            k_turning()
-            picarx_improved.stop()
+        while input_choice != 'e':
+            if input_choice == 'w':
+                self.speed = 40
+                self.angle = 0
+                self.move_forward()
+            elif input_choice == 's':
+                self.speed = 40
+                self.move_backward()
+            elif input_choice == 'a':
+                self.speed = 40
+                self.angle = -30
+                self.move_left()
+            elif input_choice == 'd':
+                self.speed = 40
+                self.angle = 30
+                self.move_right()
+            elif input_choice == 'p':
+                get_dir = input("'r' for right park and 'l' for left park")
+                self.parallel_parking(get_dir)
+            elif input_choice == 'k':
+                get_dir = input("'r' for right turn and 'l' for left turn")
+                self.k_turning(get_dir)
 
-        picarx_improved.set_dir_servo_angle(angle)
-        picarx_improved.forward(speed)
-        sleep(1)
-        input_choice = input("Use 'w-s-a-d' for 'forward-backward-left-right' maneuvering. 'e' to exit maneuvering, 'p' to park and 'k' for k_turn")
+            input_choice = input("Next Action please : ")
 
 
 if __name__ == "__main__":
-    keyboard_input()
+    driver = picar_driver()
+    driver.keyboard_input()
--- picarx_improved.py
+++ picarx_improved.py
@@ -2,6 +2,7 @@
 import time
 import logging
 import atexit
+import math
 from logdecorator import log_on_start, log_on_end, log_on_error
 
 logging_format = "%( asctime)s: %( message)s"
@@ -17,202 +18,194 @@ except ImportError:
     print("This computer does not appear to be a PiCar-X system (/opt/ezblock is not present). Shadowing hardware calls with substitute functions")
     from sim_ezblock import *
 
-PERIOD = 4095
-PRESCALER = 10
-TIMEOUT = 0.02
+class picarx_improved:
+    def __init__(self):
+        self.PERIOD = 4095
+        self.PRESCALER = 10
+        self.TIMEOUT = 0.02
 
-dir_servo_pin = Servo(PWM('P2'))
-camera_servo_pin1 = Servo(PWM('P0'))
-camera_servo_pin2 = Servo(PWM('P1'))
-left_rear_pwm_pin = PWM("P13")
-right_rear_pwm_pin = PWM("P12")
-left_rear_dir_pin = Pin("D4")
-right_rear_dir_pin = Pin("D5")
+        self.dir_servo_pin = Servo(PWM('P2'))
+        self.camera_servo_pin1 = Servo(PWM('P0'))
+        self.camera_servo_pin2 = Servo(PWM('P1'))
+        self.left_rear_pwm_pin = PWM("P13")
+        self.right_rear_pwm_pin = PWM("P12")
+        self.left_rear_dir_pin = Pin("D4")
+        self.right_rear_dir_pin = Pin("D5")
 
-S0 = ADC('A0')
-S1 = ADC('A1')
-S2 = ADC('A2')
+        self.S0 = ADC('A0')
+        self.S1 = ADC('A1')
+        self.S2 = ADC('A2')
 
-Servo_dir_flag = 1
-dir_cal_value = 0
-cam_cal_value_1 = 0
-cam_cal_value_2 = 0
-motor_direction_pins = [left_rear_dir_pin, right_rear_dir_pin]
-motor_speed_pins = [left_rear_pwm_pin, right_rear_pwm_pin]
-cali_dir_value = [1, -1]
-cali_speed_value = [0, 0]
+        self.Servo_dir_flag = 1
+        self.dir_cal_value = 0
+        self.cam_cal_value_1 = 0
+        self.cam_cal_value_2 = 0
+        self.motor_direction_pins = [left_rear_dir_pin, right_rear_dir_pin]
+        self.motor_speed_pins = [left_rear_pwm_pin, right_rear_pwm_pin]
+        self.cali_dir_value = [1, -1]
+        self.cali_speed_value = [0, 0]
 
-@log_on_start(logging.DEBUG , "stopping motors")
-@log_on_error(logging.DEBUG , "error stopping motors")
-@log_on_end(logging.DEBUG , "stopped motors successfully")
-def stop_motors():
-    set_motor_speed(1, 0)
-    set_motor_speed(2, 0)
-
-
-for pin in motor_speed_pins:
-    pin.period(PERIOD)
-    pin.prescaler(PRESCALER)
+        for pin in self.motor_speed_pins:
+            pin.period(self.PERIOD)
+            pin.prescaler(self.PRESCALER)
 
 
-def set_motor_speed(motor, speed):
-    global cali_speed_value, cali_dir_value
-    motor -= 1
-    if speed >= 0:
-        direction = 1 * cali_dir_value[motor]
-    elif speed < 0:
-        direction = -1 * cali_dir_value[motor]
-    speed = abs(speed)
-    # if speed != 0:
-    #     speed = int(speed / 2) + 50
-    speed = speed - cali_speed_value[motor]
-    if direction < 0:
-        motor_direction_pins[motor].high()
-        motor_speed_pins[motor].pulse_width_percent(speed)
-    else:
-        motor_direction_pins[motor].low()
-        motor_speed_pins[motor].pulse_width_percent(speed)
+    @log_on_start(logging.DEBUG , "stopping motors")
+    @log_on_error(logging.DEBUG , "error stopping motors")
+    @log_on_end(logging.DEBUG , "stopped motors successfully")
+    def stop_motors(self):
+        self.set_motor_speed(1, 0)
+        self.set_motor_speed(2, 0)
+
+    def set_motor_speed(self, motor, speed):
+        motor -= 1
+        if speed >= 0:
+            direction = 1 * self.cali_dir_value[motor]
+        elif speed < 0:
+            direction = -1 * self.cali_dir_value[motor]
+        speed = abs(speed)
+        # if speed != 0:
+        #     speed = int(speed / 2) + 50
+        speed = speed - self.cali_speed_value[motor]
+        if direction < 0:
+            self.motor_direction_pins[motor].high()
+            self.motor_speed_pins[motor].pulse_width_percent(speed)
+        else:
+            self.motor_direction_pins[motor].low()
+            self.motor_speed_pins[motor].pulse_width_percent(speed)
 
 
-def motor_speed_calibration(value):
-    global cali_speed_value, cali_dir_value
-    cali_speed_value = value
-    if value < 0:
-        cali_speed_value[0] = 0
-        cali_speed_value[1] = abs(cali_speed_value)
-    else:
-        cali_speed_value[0] = abs(cali_speed_value)
-        cali_speed_value[1] = 0
+    def motor_speed_calibration(self, value):
+        self.cali_speed_value = value
+        if value < 0:
+            self.cali_speed_value[0] = 0
+            self.cali_speed_value[1] = abs(self.cali_speed_value)
+        else:
+            self.cali_speed_value[0] = abs(self.cali_speed_value)
+            self.cali_speed_value[1] = 0
 
 
-def motor_direction_calibration(motor, value):
-    # 0: positive direction
-    # 1:negative direction
-    global cali_dir_value
-    motor -= 1
-    if value == 1:
-        cali_dir_value[motor] = -1 * cali_dir_value[motor]
+    def motor_direction_calibration(self, motor, value):
+        # 0: positive direction
+        # 1:negative direction
+        motor -= 1
+        if value == 1:
+            self.cali_dir_value[motor] = -1 * self.cali_dir_value[motor]
 
 
-def dir_servo_angle_calibration(value):
-    global dir_cal_value
-    dir_cal_value = value
-    set_dir_servo_angle(dir_cal_value)
-    # dir_servo_pin.angle(dir_cal_value)
+    def dir_servo_angle_calibration(self, value):
+        self.dir_cal_value = value
+        self.set_dir_servo_angle(self.dir_cal_value)
+        # dir_servo_pin.angle(dir_cal_value)
 
 
-def set_dir_servo_angle(value):
-    global dir_cal_value
-    dir_servo_pin.angle(value + dir_cal_value)
+    def set_dir_servo_angle(self, value):
+        self.dir_servo_pin.angle(value + self.dir_cal_value)
 
 
-def camera_servo1_angle_calibration(value):
-    global cam_cal_value_1
-    cam_cal_value_1 = value
-    set_camera_servo1_angle(cam_cal_value_1)
-    # camera_servo_pin1.angle(cam_cal_value)
+    def camera_servo1_angle_calibration(self, value):
+        self.cam_cal_value_1 = value
+        self.set_camera_servo1_angle(self.cam_cal_value_1)
+        # camera_servo_pin1.angle(cam_cal_value)
 
 
-def camera_servo2_angle_calibration(value):
-    global cam_cal_value_2
-    cam_cal_value_2 = value
-    set_camera_servo2_angle(cam_cal_value_2)
-    # camera_servo_pin2.angle(cam_cal_value)
+    def camera_servo2_angle_calibration(self, value):
+        self.cam_cal_value_2 = value
+        self.set_camera_servo2_angle(self.cam_cal_value_2)
+        # camera_servo_pin2.angle(cam_cal_value)
 
 
-def set_camera_servo1_angle(value):
-    global cam_cal_value_1
-    camera_servo_pin1.angle(-1 * (value + cam_cal_value_1))
+    def set_camera_servo1_angle(self, value):
+        self.camera_servo_pin1.angle(-1 * (value + self.cam_cal_value_1))
 
 
-def set_camera_servo2_angle(value):
-    global cam_cal_value_2
-    camera_servo_pin2.angle(-1 * (value + cam_cal_value_2))
+    def set_camera_servo2_angle(self, value):
+        self.camera_servo_pin2.angle(-1 * (value + self.cam_cal_value_2))
 
 
-def get_adc_value():
-    adc_value_list = []
-    adc_value_list.append(S0.read())
-    adc_value_list.append(S1.read())
-    adc_value_list.append(S2.read())
-    return adc_value_list
+    def get_adc_value(self):
+        adc_value_list = []
+        adc_value_list.append(S0.read())
+        adc_value_list.append(S1.read())
+        adc_value_list.append(S2.read())
+        return adc_value_list
 
 
-def set_power(speed):
-    set_motor_speed(1, speed)
-    set_motor_speed(2, speed)
+    def set_power(self, speed):
+        self.set_motor_speed(1, speed)
+        self.set_motor_speed(2, speed)
 
 
-def backward(speed):
-
-    set_motor_speed(1, speed)
-    set_motor_speed(2, speed)
-    print(backward)
-
-
-def forward(speed, steering_angle = 0):
-    car_length = 10
-    car_width = 11
-    radius =  car_length*math.cos(steering_angle)
-    wheelr_1 = radius + (car_width/2)
-    wheelr_2 = radius - (car_width/2)
-
-    speed_1 = speed*radius/wheelr_1
-    print("forward")
-    speed_2 = speed*radius/wheelr_2
-    print(speed_1)
-    print(speed_2)
-    set_motor_speed(1, -speed)
-    set_motor_speed(2, -speed/2)
+    def backward(self, speed):
+        logging.debug("backward")
+        self.set_motor_speed(1, speed)
+        self.set_motor_speed(2, speed)
 
 
-def stop():
-    set_motor_speed(1, 0)
-    set_motor_speed(2, 0)
+    def forward(self, speed, steering_angle = 0):
+        logging.debug("forward")
+        car_length = 10
+        car_width = 11
+        radius =  car_length/math.cos(steering_angle)
+        wheelr_1 = radius + (car_width/2)
+        wheelr_2 = radius - (car_width/2)
+
+        speed_1 = -1*(speed*radius/wheelr_1)
+        speed_2 = -1*(speed*radius/wheelr_2)
+        print(speed_1)
+        print(speed_2)
+        self.set_motor_speed(1, speed_1)
+        self.set_motor_speed(2, speed_2)
 
 
-def Get_distance():
-    timeout = 0.01
-    trig = Pin('D8')
-    echo = Pin('D9')
-
-    trig.low()
-    time.sleep(0.01)
-    trig.high()
-    time.sleep(0.000015)
-    trig.low()
-    pulse_end = 0
-    pulse_start = 0
-    timeout_start = time.time()
-    while echo.value() == 0:
-        pulse_start = time.time()
-        if pulse_start - timeout_start > timeout:
-            return -1
-    while echo.value() == 1:
-        pulse_end = time.time()
-        if pulse_end - timeout_start > timeout:
-            return -2
-    during = pulse_end - pulse_start
-    cm = round(during * 340 / 2 * 100, 2)
-    # print(cm)
-    return cm
+    def stop(self):
+        self.set_motor_speed(1, 0)
+        self.set_motor_speed(2, 0)
 
 
-def test():
-    dir_servo_angle_calibration(-10)
-    set_dir_servo_angle(-40)
+    def Get_distance(self):
+        timeout = 0.01
+        trig = ezblock.Pin('D8')
+        echo = ezblock.Pin('D9')
+
+        trig.low()
+        time.sleep(0.01)
+        trig.high()
+        time.sleep(0.000015)
+        trig.low()
+        pulse_end = 0
+        pulse_start = 0
+        timeout_start = time.time()
+        while echo.value() == 0:
+            pulse_start = time.time()
+            if pulse_start - timeout_start > timeout:
+                return -1
+        while echo.value() == 1:
+            pulse_end = time.time()
+            if pulse_end - timeout_start > timeout:
+                return -2
+        during = pulse_end - pulse_start
+        cm = round(during * 340 / 2 * 100, 2)
+        # print(cm)
+        return cm
+
+
+def test(picarx_improved):
+    picarx_improved.dir_servo_angle_calibration(-10)
+    picarx_improved.set_dir_servo_angle(-40)
     time.sleep(1)
-    set_dir_servo_angle(0)
+    picarx_improved.set_dir_servo_angle(0)
     time.sleep(1)
-    set_motor_speed(1, 1)
-    set_motor_speed(2, 1)
+    picarx_improved.set_motor_speed(1, 1)
+    picarx_improved.set_motor_speed(2, 1)
     atexit.register(stop_motors)
     # camera_servo_pin.angle(0)
 
 if __name__ == "__main__":
+    picar = picarx_improved()
     try:
-        dir_servo_angle_calibration(-10)
+        picar.dir_servo_angle_calibration(-10)
         while 1:
-            test()
+            test(picar)
     finally:
-        stop()
+        picar.stop()
